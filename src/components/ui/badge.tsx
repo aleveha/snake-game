45f10f6064b6e610447dc16dd3b6f249b933dbb0
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-11-21 12:31:20 +0100

    feat: remove unused components

--- src/components/ui/badge.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-
-interface Props {
-	children: React.ReactNode;
-}
-
-export const Badge: FC<Props> = ({ children }) => {
-	return <span className="rounded-xl border bg-green-600 px-3 py-2 text-white">{children}</span>;
-};
