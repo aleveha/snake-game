36c3bfb98a2b77f8af3f9ac005879e05960e0cab
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-11-21 12:29:43 +0100

    feat: move coordinates to server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
 import { Page } from "@src/pages/homepage";
+import { AnswerCoordinates } from "@src/components/board/types";
+
+const coordinates: AnswerCoordinates = {
+	latitude: {
+		degrees: 11,
+		minutes: 16,
+		seconds: 4.6,
+	},
+	longitude: {
+		degrees: 32,
+		minutes: 3,
+		seconds: 34.8,
+	},
+};
+
+export const getStaticProps = async () => {
+	return {
+		props: {
+			coordinates,
+		},
+	};
+};
 
 export default Page;
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -3,20 +3,11 @@ import { Intro } from "@src/components/intro";
 import { Board } from "@src/components/board/board";
 import { AnswerCoordinates } from "@src/components/board/types";
 
-const coordinates: AnswerCoordinates = {
-	latitude: {
-		degrees: 41,
-		minutes: 16,
-		seconds: 4.6,
-	},
-	longitude: {
-		degrees: 32,
-		minutes: 3,
-		seconds: 34.8,
-	},
-};
+export interface HomePageProps {
+	coordinates: AnswerCoordinates;
+}
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage<HomePageProps> = ({ coordinates }) => {
 	return (
 		<div className="ui-container space-y-8 py-8">
 			<Intro />
